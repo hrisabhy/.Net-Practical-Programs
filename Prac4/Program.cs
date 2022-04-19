54973eb31d6820f9b2ff352a6c7ad31c9396d62e
hrisabhy <hrisabhyadav31@gmail.com>
2022-04-19 19:17:10 +0530

    .

--- Prac4/Program.cs
+++ Prac4/Program.cs
@@ -2,11 +2,29 @@
 
 namespace Prac4
 {
+    class Animal
+    {
+        public void eat()
+        {
+            Console.WriteLine("Eating");
+        }
+    }
+    class Dog : Animal
+    {
+        public void bark()
+        {
+            Console.WriteLine("Barking");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Animal animal1 = new Animal();
+            Dog dog1 = new Dog();
+            dog1.eat();
+            dog1.bark();
+            
         }
     }
 }
