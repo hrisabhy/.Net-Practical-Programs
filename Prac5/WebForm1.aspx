499e1d9525c3bf50b3a56eaded7229cfc1d159ae
hrisabhy <hrisabhyadav31@gmail.com>
2022-04-19 19:38:10 +0530

    .

---
+++ Prac5/WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Prac5.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Enter Your Number:<asp:TextBox ID="getNum" runat="server"></asp:TextBox>
+        </div>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Check Palimdrome" />
+        </p>
+        <p>
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Prac5/WebForm1.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Prac5
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int num = int.Parse(getNum.Text);
+            int n, rev = 0, d;
+            n = num;
+            while (n > 0)
+            {
+                d = n % 10;
+                n = n / 10;
+                rev = rev * 10 + d;
+
+            }
+            if (rev == num)
+            {
+                resultLabel.Text = "Is a palimdrome";
+            }
+            else
+            {
+                resultLabel.Text = "not a plalindrome";
+            }
+        }
+    }
+}
